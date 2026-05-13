9554424d7a7550b80d723c6fc0abccd661eb507a
Kennedy Luchembe Kabwe <kabwekennedy023@gmail.com>
2026-05-13 13:12:41 +0200

    Add SensorLog header and implementation skeleton

---
+++ include/SensorLog.h
@@ -0,0 +1,17 @@
+#ifndef  SENSORLOG_H
+#define  SENSORLOG_H
+#include<iostream>
+#include <vector>
+#include "SensorReading.h"
+
+class SensorLog {
+    private:
+        std::vector<SensorReading> readings; //container for all sensor readings
+    public:
+
+
+};
+
+
+
+#endif // SENSORLOG_H
